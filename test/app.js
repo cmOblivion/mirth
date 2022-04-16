9a34f8879b35929dcb761c8bc4765542184cb2fb
cm <chenzihandawang@163.com>
2022-04-16 16:51:56 +0800

    addVideo

--- test/app.js
+++ test/app.js
@@ -3,10 +3,8 @@ const mirth = require('./../mirth');
 var sv = mirth.Server()
 	.route({
 		path:/^\/$/i,
-		render:new mirth.response.JsonResponse({
-			data(){
-				return {test:'ha!!'};
-			}
+		render:new mirth.response.HtmlResponse({
+			filepath:'tmp/index.html'
 		})
 	})
 	.static()
